d1369774e00d5dfd5f251822eed27bb51655dd44
Nishad Agrawal <nishadagrawal7@gmail.com>
2014-11-15 15:26:07 -0500

    testing firebase

---
+++ app/src/main/java/com/jtna/holyshift/backend/User.java
@@ -0,0 +1,13 @@
+package com.jtna.holyshift.backend;
+
+/**
+ * Created by Nishad Agrawal on 11/15/14.
+ */
+public class User {
+    private String fullName;
+    private String userName;
+    private String password;
+    private List<Group> myGroups;
+    private Availablility myAvail;
+
+}
