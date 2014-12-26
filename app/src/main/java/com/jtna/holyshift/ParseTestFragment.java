c98745a2476f9ed9143a69d7011b659087627f98
Nishad Agrawal <nishadagrawal7@gmail.com>
2014-12-26 00:29:47 -0600

    implementation of test fragment

--- app/src/main/java/com/jtna/holyshift/ParseTestFragment.java
+++ app/src/main/java/com/jtna/holyshift/ParseTestFragment.java
@@ -1,24 +1,46 @@
 package com.jtna.holyshift;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.jtna.holyshift.backend.Availability;
+import com.jtna.holyshift.backend.Day;
+import com.jtna.holyshift.backend.Group;
+import com.jtna.holyshift.backend.Shift;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ParseTestFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link ParseTestFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class ParseTestFragment extends Fragment {
+    public static final String NUM_REGEX = "-?[0-9]+\\.?[0-9]*";
 
-    private OnFragmentInteractionListener mListener;
+    private Spinner daySpinner;
+    private EditText shiftStartEditText;
+    private EditText numberRequiredEditText;
+    private Button setShiftButton;
+    private EditText groupNameEditText;
+    private EditText passwordEditText;
+    private Button createGroupButton;
+    private Button setAvailabilityButton;
+    private Button addUserButton;
+    private Button assignShiftsButton;
+    private List<Shift> myShifts;
+    private Group myGroup;
 
     /**
      * Use this factory method to create a new instance of
@@ -43,7 +65,86 @@ public class ParseTestFragment extends Fragment {
     }
 
     private void initializeComponents(View rootView) {
-        
+        daySpinner = (Spinner) rootView.findViewById(R.id.day_spinner);
+        daySpinner.setAdapter(new ArrayAdapter<>(this.getActivity(),
+                android.R.layout.simple_spinner_item, Day.values()));
+
+        shiftStartEditText = (EditText) rootView.findViewById(R.id.shiftStartEditText);
+        numberRequiredEditText = (EditText) rootView.findViewById(R.id.numberRequiredEditText);
+
+        myShifts = new ArrayList<>();
+        setShiftButton = (Button) rootView.findViewById(R.id.setShiftButton);
+        setShiftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (daySpinner.getSelectedItem() != null &&
+                        !shiftStartEditText.getText().toString().isEmpty() &&
+                        Pattern.matches(NUM_REGEX, shiftStartEditText.getText().toString()) &&
+                        !numberRequiredEditText.getText().toString().isEmpty() &&
+                        Pattern.matches(NUM_REGEX, numberRequiredEditText.getText().toString())) {
+                    Shift s = new Shift((Day) daySpinner.getSelectedItem(),
+                            Integer.parseInt(shiftStartEditText.getText().toString()),
+                            Integer.parseInt(numberRequiredEditText.getText().toString()));
+                    s.saveInBackground();
+                    myShifts.clear();
+                    myShifts.add(s);
+                    Log.d("PARSE TEST", "created new shift");
+                }
+            }
+        });
+
+
+        groupNameEditText = (EditText) rootView.findViewById(R.id.groupNameEditText);
+        passwordEditText = (EditText) rootView.findViewById(R.id.passwordEditText);
+        createGroupButton = (Button) rootView.findViewById(R.id.createGroupButton);
+        createGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!groupNameEditText.getText().toString().isEmpty() &&
+                        !passwordEditText.getText().toString().isEmpty() &&
+                        !myShifts.isEmpty()) {
+                    myGroup = new Group(groupNameEditText.getText().toString(),
+                            passwordEditText.getText().toString(),
+                            myShifts);
+                    myGroup.saveInBackground();
+                    Log.d("PARSE TEST", "group set");
+                }
+            }
+        });
+
+        setAvailabilityButton = (Button) rootView.findViewById(R.id.setAvailButton);
+        setAvailabilityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Availability avail = new Availability();
+                avail.saveInBackground();
+                Log.d("PARSE TEST", "created user availability");
+                avail.setUser(ParseUser.getCurrentUser());
+                avail.saveInBackground();
+                Log.d("PARSE TEST", "set avail parse user to me");
+            }
+        });
+
+        addUserButton = (Button) rootView.findViewById(R.id.addUserButton);
+        addUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (myGroup != null) {
+                    myGroup.addUser(ParseUser.getCurrentUser());
+                    myGroup.saveInBackground();
+                    Log.d("PARSE TEST", "user added to group");
+                }
+            }
+        });
+
+        assignShiftsButton = (Button) rootView.findViewById(R.id.assignShiftsButton);
+        assignShiftsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+                Log.e("PARSE TEST", "to be implemented");
+            }
+        });
     }
 
     @Override
@@ -52,38 +153,6 @@ public class ParseTestFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_parse_test, container, false);
         initializeComponents(rootView);
         return rootView;
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 
 }
